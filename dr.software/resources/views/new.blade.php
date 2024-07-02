158c855384ec3bbf487e621e16d7a78a18e2ab36
Eyupispir <Eypispir@outlook.com>
2024-07-02 09:56:37 +0300

    no changes

---
+++ dr.software/resources/views/new.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="row">
+    <div class="col text-center">
+        <a class="btn btn-info " href="{{ route('new') }}">new game</a>
+    </div>
+</div>
+
+@endsection
